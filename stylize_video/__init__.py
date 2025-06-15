e8f881829189eac542cc1c7d36f5ca5dacd26a02
Roland Van Duine <69098858+colmak@users.noreply.github.com>
2025-06-15 17:57:46 -0400

    init

---
+++ stylize_video/__init__.py
@@ -0,0 +1,8 @@
+"""
+Stylize Video - Neural Style Transfer for Videos
+
+Transform your videos into artistic masterpieces using deep neural networks.
+"""
+
+__version__ = "0.1.0"
+__author__ = "Style Transfer Video Team"
