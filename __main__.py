35768753210f30e4659e2491fa1892cce7d848fe
Roland Van Duine <69098858+colmak@users.noreply.github.com>
2025-06-15 17:57:17 -0400

    init

---
+++ __main__.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+"""
+Style Transfer Video CLI Tool
+
+This tool applies neural style transfer to videos, transforming ordinary footage
+into artistic masterpieces using various pre-trained models.
+"""
+
+if __name__ == "__main__":
+    from stylize_video.cli import cli
+    cli()
